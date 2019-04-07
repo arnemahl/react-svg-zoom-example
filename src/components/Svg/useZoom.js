cd7d842fe2d3b73789c190a31b7a733f481690fb
Arne Mæhlum <arne.mahlum@gmail.com>
2019-04-07 13:22:49 +0200

    fix: When zooming out, don't show outside of full view

--- src/components/Svg/useZoom.js
+++ src/components/Svg/useZoom.js
@@ -5,7 +5,7 @@ export function useZoom(viewBox, setViewBox) {
   const onWheel = React.useCallback((event) => {
     const d = event.deltaY / 10
     const [x, y, width, height] = viewBox
-    const [nextX, nextY, nextWidth, nextHeight] = [x - d / 2, y - d / 2, width + d, height + d]
+    let [nextX, nextY, nextWidth, nextHeight] = [x - d / 2, y - d / 2, width + d, height + d]
 
     // Prevent zooming too far in
     if (nextWidth <= 0 || nextHeight <= 0) {
@@ -15,6 +15,21 @@ export function useZoom(viewBox, setViewBox) {
     // Prevent zooming too far out
     if (nextWidth > fullWidth || nextHeight > fullHeight) {
       return
+    }
+
+    // Prevent zooming out to the side
+    // (When zooming out, go back to original viewBox so we keep stuff centerd)
+    if (nextX < 0) {
+      nextX = 0
+    }
+    if (nextY < 0) {
+      nextY = 0
+    }
+    if (nextX + nextWidth > fullWidth) {
+      nextX = fullWidth - nextWidth
+    }
+    if (nextY + nextHeight > fullHeight) {
+      nextY = fullHeight - nextHeight
     }
 
     setViewBox([nextX, nextY, nextWidth, nextHeight])
