813c48dfd56ada044253a3b54c8ef2c13c94e21b
Arne Mæhlum <arne.mahlum@gmail.com>
2019-04-07 12:51:51 +0200

    refactor: Make custom usePan hook

--- src/components/Svg/Svg.js
+++ src/components/Svg/Svg.js
@@ -3,27 +3,7 @@ import React from 'react';
 const fullWidth = 100;
 const fullHeight = 100;
 
-export default function App() {
-  const [viewBox, setViewBox] = React.useState([0, 0, fullWidth, fullHeight])
-
-  const onWheel = React.useCallback((event) => {
-    const d = event.deltaY / 10
-    const [x, y, width, height] = viewBox
-    const nextViewBox = [x - d / 2, y - d / 2, width + d, height + d]
-
-    // Prevent zooming too far in
-    if (nextViewBox[3] <= 0 || nextViewBox[4] <= 0) {
-      return
-    }
-
-    // Prevent zooming too far out
-    if (nextViewBox[3] > fullWidth || nextViewBox[4] <= fullHeight) {
-      return
-    }
-
-    setViewBox(nextViewBox)
-  }, [viewBox, setViewBox])
-
+function usePan(viewBox, setViewBox) {
   const onMouseDown = React.useCallback((event) => {
     function onMouseMove(event) {
       const { movementX: dx, movementY: dy } = event
@@ -50,6 +30,32 @@ export default function App() {
     window.addEventListener("mousemove", onMouseMove)
     window.addEventListener("mouseup", onMouseUp)
   }, [viewBox, setViewBox])
+
+  return { onMouseDown }
+}
+
+export default function App() {
+  const [viewBox, setViewBox] = React.useState([0, 0, fullWidth, fullHeight])
+
+  const onWheel = React.useCallback((event) => {
+    const d = event.deltaY / 10
+    const [x, y, width, height] = viewBox
+    const nextViewBox = [x - d / 2, y - d / 2, width + d, height + d]
+
+    // Prevent zooming too far in
+    if (nextViewBox[3] <= 0 || nextViewBox[4] <= 0) {
+      return
+    }
+
+    // Prevent zooming too far out
+    if (nextViewBox[3] > fullWidth || nextViewBox[4] <= fullHeight) {
+      return
+    }
+
+    setViewBox(nextViewBox)
+  }, [viewBox, setViewBox])
+
+  const { onMouseDown } = usePan(viewBox, setViewBox)
 
   return (
     <svg
