4b106e52749d6e04ef618dec4fe46f3ecb6f88f0
larseen <kristoffer@larsen.so>
2019-04-17 20:19:47 +0200

    Add trash

--- src/components/Svg/Svg.js
+++ src/components/Svg/Svg.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { useZoom } from "./useZoom";
+import useKey from "./useKey";
 import { usePan } from "./usePan";
 
-export default function App({ width, height }) {
+export default function App({ width, height, resetViewbox }) {
   const [viewBox, setViewBox] = React.useState([0, 0, width, height]);
   const { onWheel, zoom } = useZoom(viewBox, setViewBox, width, height);
-  const { onMouseDown } = usePan(viewBox, setViewBox, width, height);
+  const { onMouseDown: pan } = usePan(viewBox, setViewBox, width, height);
+  //const { onMouseDown: draw, element } = draw(viewBox, width, height); TODO
+  const modifier = useKey("Shift");
 
   return (
     <svg
@@ -13,13 +16,16 @@ export default function App({ width, height }) {
       height={`${height}px`}
       viewBox={viewBox.join(" ")}
       onWheel={onWheel}
-      onMouseDown={onMouseDown}
+      onMouseDown={e => {
+        if (modifier) pan(e);
+        //  else draw(e);
+      }}
       onDoubleClick={e => {
         setViewBox([0, 0, width, height]);
       }}
     >
       <image
-        href="https://3lhowb48prep40031529g5yj-wpengine.netdna-ssl.com/wp-content/uploads/2012/09/photo22.jpg"
+        href="img.jpg"
         x={0}
         y={0}
         width={`${width}px`}
